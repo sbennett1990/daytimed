8a32c09817d4f05258626587d9efdfddb9fe7e8c
Scott Bennett <scottb@fastmail.com>
2020-04-06 17:18:40 -0400

    Import initial skeletal program which listens for connections and prints the time

---
+++ datetimed.c
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2020 Scott Bennett <scottb@fastmail.com>
+ */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include <err.h>
+#include <errno.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+int debug = 0;
+char *timeformat = "%a %b %e %H:%M:%S %Z %Y";
+
+#define DPRINTF(x...) do { if (debug) printf(x); } while (0)
+
+void
+usage()
+{
+	printf("datetimed [-d]\n");
+}
+
+/*
+ * Get the current date and time and write a human-readable string to buf.
+ */
+void
+getthetime(char *buf, size_t len)
+{
+	time_t tval;
+	if (time(&tval) == -1) {
+		err(1, "time failed");
+	}
+
+	struct tm *tp = localtime(&tval);
+	if (tp == NULL) {
+		err(1, "time conversion error");
+	}
+
+	strftime(buf, len, timeformat, tp);
+}
+
+int
+main(int argc, char **argv)
+{
+	int port = 17017;
+	struct sockaddr_in sockname, client;
+	int clientlen;
+	int sd;		/* socket descriptor */
+	char timestr[1024];
+
+	int c;
+	while ((c = getopt(argc, argv, "d")) != -1) {
+		switch (c) {
+			case 'd':
+				debug = 1;
+				break;
+			default:
+				usage();
+				break;
+		}
+	}
+	argc -= optind;
+	argv += optind;
+
+	if (argc > 1) {
+		usage();
+		exit(1);
+	}
+
+	/* set up the socket */
+	memset(&sockname, 0, sizeof(sockname));
+	sockname.sin_family = AF_INET;
+	sockname.sin_port = htons(port);
+	sockname.sin_addr.s_addr = htonl(INADDR_ANY);
+	sd = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (sd == -1)
+		err(1, "socket failed");
+	if (bind(sd, (struct sockaddr *)&sockname, sizeof(sockname)) == -1)
+		err(1, "bind failed");
+	if (listen(sd, 3) == -1)
+		err(1, "listen failed");
+
+	DPRINTF("server up and listening for connections on port %d\n", port);
+
+	for (;;) {
+		int clientsd;
+		clientlen = sizeof(&client);
+		clientsd = accept(sd, (struct sockaddr *)&client, &clientlen);
+		if (clientsd == -1)
+			err(1, "accept failed");
+
+		getthetime(timestr, sizeof(timestr));
+		printf("%s\n", timestr);
+		
+		close(clientsd);
+	}
+}
