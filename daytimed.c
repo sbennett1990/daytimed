50b09d168fc2166f19c3c6fe8c85d4e0ff454645
Scott Bennett <scottb@fastmail.com>
2022-07-19 17:56:41 -0400

    the length should be a socklen_t

--- daytimed.c
+++ daytimed.c
@@ -198,7 +198,7 @@ main(int argc, char **argv)
 	char timestr[256];		/* holds the time string sent to the client */
 	struct sockaddr_in clientsock;
 	for (;;) {
-		int clientsocklen = sizeof(&clientsock);
+		socklen_t clientsocklen = sizeof(&clientsock);
 		int clientsd = accept(listensd, (struct sockaddr *)&clientsock, &clientsocklen);
 		if (clientsd == -1) {
 			err(1, "accept failed");
